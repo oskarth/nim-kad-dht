ab5df8dbf20de2612087cd3eb4b3fe25034881b2
Oskar Thoren <ot@oskarthoren.com>
2020-01-17 16:08:53 +0800

    Basic xor

--- src/kademlia.nim
+++ src/kademlia.nim
@@ -4,14 +4,14 @@ import random
 
 const
   alpha = 3 # degree of parallelism in network calls
-  b = 160   # size of bits of keys used to identify nodes and data
+  b = 3 # for testing
+  #b = 160   # size of bits of keys used to identify nodes and data
   k = 20    # maximum number of contacts stored in a bucket
 
 # TODO: tExpire, tRefresh, tReplicate, tRepublish constants
 
 type
-  Bit = range[0..1]
-  NodeID* = array[b, Bit]
+  NodeID* = array[b, int]
 
 proc genNodeID(): NodeID =
   for i in 0..<result.len:
@@ -29,4 +29,15 @@ proc printNodeID(nodeID: NodeID) =
 var nodeID = genNodeID()
 printNodeID(nodeID)
 
-# TODO Distance function
+proc distance(a: NodeID, b: NodeID): NodeID = 
+  for i in 0..<result.len:
+    result[i] = a[i] xor b[i]
+
+var anode = genNodeID()
+var bnode = genNodeID()
+printNodeID(anode)
+printNodeID(bnode)
+printNodeID(distance(anode,bnode))
+
+#echo("xor", (0b0011 xor 0b0101))
+#echo("bit array?", 0b010, " : ", type(0b010))
