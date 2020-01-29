f6eed2d83a2b97658834b6caab3349febcdbb499
Oskar Thoren <ot@oskarthoren.com>
2020-01-29 15:45:32 +0800

    Add first KadPeer to kbucket as contact

--- src/libp2pkad.nim
+++ src/libp2pkad.nim
@@ -1,3 +1,4 @@
+import strutils
 import tables
 import chronos
 import libp2p/[switch,
@@ -20,16 +21,20 @@ import libp2p/[switch,
 const KadCodec = "/test/kademlia/1.0.0" # custom protocol string
 
 const k = 2 # maximum number of peers in bucket, test setting, should be 20
-const b = 160 # size of bits of keys used to identify nodes and data
+const b = 271 # size of bits of keys used to identify nodes and data
+
+# b is based on PeerID size: 8*34-1 = 271
 
 # Should parameterize by b, size of bits of keys (Peer ID dependent?)
 type
   KadPeer = ref object of RootObj
     peerInfo: PeerInfo
-  KBucket = array[k, KadPeer] # should be k length
+  KBucket = seq[KadPeer] # should be k length
+  #KBucket = array[k, KadPeer] # should be k length
+  KBuckets = array[b, KBucket] # should be k length
   KadProto* = ref object of LPProtocol # declare a custom protocol
     peerInfo: PeerInfo # this peer's info, should be b length
-    Kbuckets: array[b, KBucket] # should be b length
+    kbuckets: KBuckets # should be b length
 
 # Returns XOR distance as PeerID
 # Assuming these are of equal length, b
@@ -40,22 +45,48 @@ proc xor_distance(a, b: PeerID): PeerID =
     data.add(a.data[i] xor b.data[i])
   return PeerID(data: data)
 
-method init(p: KadProto, peerInfo: PeerInfo) {.gcsafe.} =
+# Finds kbucket to place peer in by returning most significant bit position
+# Assumes bigendian and byte=uint8 (2^8-1)
+# Note that PeerId 8*34 = 272, which is bigger than b=160
+method which_kbucket(p: KadProto, contact: PeerInfo): int {.base.} =
+  var bs: string # helper bit string
+  var d = xor_distance(p.peerInfo.peerId, contact.peerId)
+  for i in 0..<d.data.len:
+    bs = bs & ord(d.data[i]).toBin(8)
+  #echo ("bs: ", bs)
+  for i in 0..bs.len:
+    if bs[i] == '1':
+      return bs.len - 1 - i
+  # Self, not really a bucket, better error type
+  return -1
+
+# Here we can have different types of Kademlia, like forwarding and classical
+#proc newKad(peerInfo: PeerInfo): KadProto =
+#  new result
+#  result.peerInfo = peerInfo
+#  #result.initKad()
+
+method init(p: KadProto, peerInfo: PeerInfo) {.base, gcsafe.} =
   # handle incoming connections in closure
   proc handle(conn: Connection, proto: string) {.async, gcsafe.} =
     echo "Got from remote - ", cast[string](await conn.readLp())
     await conn.writeLp("Hello!")
     await conn.close()
 
+  var kbuckets: KBuckets
+
   p.peerInfo = peerInfo
   p.codec = KadCodec # init proto with the correct string id
   p.handler = handle # set proto handler
+  p.kbuckets = kbuckets
 
-# TODO: Setup kbuckets, fix byte to bit to know which contact
-method addContact(p: KadProto, contact: PeerInfo) {.gcsafe.} =
-  echo ("addContact ", contact)
-  # Find which_kbucket(self, c.id)
-  #p.kbuckets[0].add(c)
+method addContact(p: KadProto, contact: PeerInfo) {.base, gcsafe.} =
+  echo("addContact ", contact)
+  var index = p.which_kbucket(contact)
+  echo("which kbucket ", index)
+
+  var kadPeer = KadPeer(peerInfo: contact)
+  p.kbuckets[index].add(kadPeer)
 
 proc createSwitch(ma: MultiAddress): (Switch, PeerInfo) =
   ## Helper to create a swith
@@ -78,7 +109,9 @@ proc createSwitch(ma: MultiAddress): (Switch, PeerInfo) =
   let muxers = {MplexCodec: mplexProvider}.toTable() # add all muxers
   let secureManagers = {SecioCodec: Secure(newSecio(seckey))}.toTable() # setup the secio and any other secure provider
 
-  # create the switch
+  # Add kadProto field to Switch type as optional? This is how pubsub works
+  # let kadProto = KadProto newKad(peerInfo)
+
   let switch = newSwitch(peerInfo,
                          transports,
                          identify,
@@ -107,6 +140,9 @@ proc main() {.async, gcsafe.} =
 
   # XOR distance between two peers
   echo("*** xor_distance ", xor_distance(peerInfo1.peerId, peerInfo2.peerId))
+
+  # TODO: HERE ATM: Print contacts table per bucket
+  kadProto.addContact(peerInfo2)
 
   await conn.writeLp("Hello!") # writeLp send a length prefixed buffer over the wire
   # readLp reads length prefixed bytes and returns a buffer without the prefix
